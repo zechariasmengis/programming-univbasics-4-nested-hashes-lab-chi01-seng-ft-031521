c3c090605897addb60fdc65672d8ce290c6bb885
Zecharias Mengis <zmengis@gmail.com>
2021-02-12 02:43:04 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,8 @@ def hopper
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:grace_hopper]
 
 end
 
@@ -36,6 +38,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,6 +58,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,12 +85,17 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    programmer_hash[:yukihiro_matsumoto] = { :known_for => "Ruby",
+    :languages => ["LISP", "C"] }
+    
+    programmer_hash
 
 end
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
+  alans_new_info = "GUI"
 
 	programmer_hash = 
  		{
@@ -103,6 +112,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash
 
 end
 
@@ -125,5 +136,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+    programmer_hash
 
 end
