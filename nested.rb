ade3f61ba474e88ec402ac661490555a50e1f22b
Zecharias Mengis <zmengis@gmail.com>
2021-02-12 02:19:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie][:languages]
+    dr_language = programmer_hash[:dennis_ritchie][:languages]
+    dr_language.to_s
 
 end
 
