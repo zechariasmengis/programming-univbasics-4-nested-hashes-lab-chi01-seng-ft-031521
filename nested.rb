f0f8b50135e991b29b35b2f4a8efeb4741e25533
Zecharias Mengis <zmengis@gmail.com>
2021-02-12 02:25:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,9 @@ def adding_matz
         :languages => ["C"]
       }
     }
-    programmer_hash[:yukihiro_matsumoto] = 
+    programmer_hash[:yukihiro_matsumoto] = { :known_for => "Ruby",
+    :languages => ["LISP", "C"] }
+    
     programmer_hash
 
 end
